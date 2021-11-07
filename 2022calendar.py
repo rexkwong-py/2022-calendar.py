73dd8ab2c820853c19ef9f1135b988dd1b0a1c7e
rex <rexkwong727@gmail.com>
2021-11-07 11:03:04 +0800

    Create 2022calendar.py

---
+++ 2022calendar.py
@@ -0,0 +1,13 @@
+import calendar
+print(calendar.month(2022,1))
+print(calendar.month(2022,2))
+print(calendar.month(2022,3))
+print(calendar.month(2022,4))
+print(calendar.month(2022,5))
+print(calendar.month(2022,6))
+print(calendar.month(2022,7))
+print(calendar.month(2022,8))
+print(calendar.month(2022,9))
+print(calendar.month(2022,10))
+print(calendar.month(2022,11))
+print(calendar.month(2022,12))
